c069fac2f796eb1d08b9995557c1a6294b6308fc
mishas8 <dexter88retxed@gmail.com>
2016-11-30 07:18:05 +0300

    Add DATA structure

--- LectionApp-Desktop/imageprovider.cpp
+++ LectionApp-Desktop/imageprovider.cpp
@@ -1,14 +1,7 @@
-#include "mainwindow.h"
-#include "ui_mainwindow.h"
+#include "imageprovider.h"
+#include <stdexcept>
+//#include "QSql"
+#include <QTreeView>
+//#include <QSqlDatabase>
 
-MainWindow::MainWindow(QWidget *parent) :
-    QMainWindow(parent),
-    ui(new Ui::MainWindow)
-{
-    ui->setupUi(this);
-}
-
-MainWindow::~MainWindow()
-{
-    delete ui;
-}
+using namespace std;
--- LectionApp-Desktop/imageprovider.h
+++ LectionApp-Desktop/imageprovider.h
@@ -1,22 +1,64 @@
-#ifndef MAINWINDOW_H
-#define MAINWINDOW_H
+#ifndef IMAGEPROVIDER_H
+#define IMAGEPROVIDER_H
+#include <QAbstractItemModel>
+#include <QTreeView>
+//#include <QSqlDatabase>
+#include <QStringList>
 
-#include <QMainWindow>
+enum dataType {ROOT, SEMESTR, COURSE, LECTURE, IMAGE};
 
-namespace Ui {
-class MainWindow;
-}
-
-class MainWindow : public QMainWindow
+struct DataWrapper // обертка
 {
-    Q_OBJECT
-
-public:
-    explicit MainWindow(QWidget *parent = 0);
-    ~MainWindow();
-
-private:
-    Ui::MainWindow *ui;
+    int id;
+    dataType type;
+    void* data;
+    int number;
+    DataWrapper* parentPointer;
+    QList <DataWrapper*> children;
+    int childrenCount;
 };
 
-#endif // MAINWINDOW_H
+struct IData // Image Data
+{
+    int id;
+    int p_id; // parentId
+    int number;
+    QString path; // path to image
+    QString comment;
+    QVector <QString> tags;
+};
+
+struct HData // Href Data
+{
+    int id;
+    int p_id;
+    QString type;
+    QString name;
+    QString comment;
+    int number;
+};
+
+class ImageProvider : public QAbstractItemModel
+{
+private:
+    DataWrapper root {0, ROOT, nullptr, 0, nullptr, {}, 0};
+    int getChildCount(dataType type, int parentId) const;
+    const DataWrapper* dataForIndex(const QModelIndex &index) const;
+    //DataWrapper* dataForIndex(const QModelIndex &index);
+
+public:
+    ImageProvider(QString dbname, QObject *parent = nullptr);
+
+    ~ImageProvider();
+
+    virtual int rowCount(const QModelIndex &parent) const;
+    virtual int columnCount(const QModelIndex &parent) const;
+    virtual QModelIndex index(int row, int column, const QModelIndex &parent) const;
+    virtual QVariant data(const QModelIndex &index, int role) const; // QVariant обертка, в нее можно завернуть объект любого типа
+    virtual QModelIndex parent(const QModelIndex &child) const;
+    void fetchAll(const QModelIndex &parent);
+    void fetchMore(const QModelIndex &parent);
+    bool canFetchMore(const QModelIndex &parent) const;
+};
+
+#endif // IMAGEPROVIDER_H
